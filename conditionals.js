af926681636cebd1e2cd5ba7a791bb602fb89df0
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2020-12-05 20:51:15 +0000

    Add more coditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,37 +1,61 @@
-// if statements
+// // if statements
 
-const raining = false;
-const cold = true;
+// const raining = false;
+// const cold = true;
 
-if (raining) {
-  console.log("Don't forget your umbrella");
-}
+// if (raining) {
+//   console.log("Don't forget your umbrella");
+// }
 
-if (cold) {
-  console.log("Make sure you pick out a scarf!");
-}
+// if (cold) {
+//   console.log("Make sure you pick out a scarf!");
+// }
 
-console.log("Now you're ready to go outside");
+// console.log("Now you're ready to go outside");
 
-// if else
+// // if else
 
-const cold = false;
+// const cold = false;
 
-if (cold) {
-  console.log("Make sure you pick out a scarf!");
-} else {
-  console.log("short sleeves are fine.");
-}
+// if (cold) {
+//   console.log("Make sure you pick out a scarf!");
+// } else {
+//   console.log("short sleeves are fine.");
+// }
 
-// if else if 
+// // if else if 
 
-const temperature = -9;
+// const temperature = -9;
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!")
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it");
-} else {
-  console.log("Short sleeves are fine");
-}
-console.log("Now you're ready to go outside!");
+// if (temperature < 0) {
+//   console.log("Make sure you pick out a scarf!")
+// } else if (temperature < 15) {
+//   console.log("Short sleeves won't cut it");
+// } else {
+//   console.log("Short sleeves are fine");
+// }
+// console.log("Now you're ready to go outside!");
+
+
+//Logical operators
+
+// const isCitizen = true;
+// const age = 26;
+
+// if (isCitizen && age > 18) {
+//   console.log("You are elegible to vote");
+// }
+
+
+
+// temperature = -40.5;
+
+// if(temperature < -40 || temperature > 40) {
+//   console.log("Maybe going outside isn't such a great idea");
+// }
+
+// const raining;
+
+// if (!raining) {
+//   console.log("Leave your umbrella at home!")
+// }
